bb3975b16f92713a34bf6df91b968db559015764
Jef Mancera <contactjefjs@gmail.com>
2025-02-17 23:12:26 -0500

    Add Radix UI form components and styles.

--- front/src/pages/leads/leads.tsx
+++ front/src/pages/leads/leads.tsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
 import { leadService } from "@/services/leadService"
 import { LeadData } from "@/services/leadService"
+import { Form, Switch } from "radix-ui";
+import "../../radix.css";
+
 
 export interface LeadsProps {
 }
@@ -36,13 +39,10 @@ export default function Leads(props?: LeadsProps) {
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>, index?: number) => {
     const { name, value } = e.target;
-    if (name.startsWith('tracking') || name.startsWith('description') ||  name.startsWith('name')) {
+    if (name.startsWith('tracking') || name.startsWith('description') || name.startsWith('name')) {
       const newTrackings = [...trackings];
       newTrackings[index!][name] = value;
-      setTrackings(newTrackings);
-    } if ( name.startsWith('new') ) {
-      const newTrackings = [...trackings];
-      newTrackings[index]['new'] = !newTrackings[index]['new'];
+      newTrackings[index!]['new'] = true;
       setTrackings(newTrackings);
     } else {
       setFormData({
@@ -57,7 +57,6 @@ export default function Leads(props?: LeadsProps) {
     try {
       const response = await leadService.create({ ...formData, trackings });
       if (response) {
-        alert('Lead creado exitosamente');
         setReload(true); // Set reload flag to true
         setFormData({
           full_name: '',
@@ -106,112 +105,220 @@ export default function Leads(props?: LeadsProps) {
     };
 
     fetchLeads();
-  }, [reload]); 
+  }, [reload]);
 
   useEffect(() => {
     if (reload) {
-      setReload(false); 
+      setReload(false);
     }
   }, [reload]);
 
+
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Nombre Completo:</label>
-          <input type="text" name="full_name" value={formData.full_name} onChange={handleChange} required />
+
+      <Form.Root className="FormRoot" onSubmit={handleSubmit}>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          <div style={{ flex: 1, paddingRight: "20px" }}>
+            <Form.Field className="FormField" name="full_name">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "baseline",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Form.Label className="FormLabel">Nombre Completo</Form.Label>
+                <Form.Message className="FormMessage" match="valueMissing">
+                  Por favor ingrese su nombre completo
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <input className="Input" type="text" name="full_name" value={formData.full_name} onChange={handleChange} required />
+              </Form.Control>
+            </Form.Field>
+            <Form.Field className="FormField" name="first_name">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "baseline",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Form.Label className="FormLabel">Nombre</Form.Label>
+                <Form.Message className="FormMessage" match="valueMissing">
+                  Por favor ingrese su nombre
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <input className="Input" type="text" name="first_name" value={formData.first_name} onChange={handleChange} required />
+              </Form.Control>
+            </Form.Field>
+            <Form.Field className="FormField" name="last_name">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "baseline",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Form.Label className="FormLabel">Apellido</Form.Label>
+                <Form.Message className="FormMessage" match="valueMissing">
+                  Por favor ingrese su apellido
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <input className="Input" type="text" name="last_name" value={formData.last_name} onChange={handleChange} required />
+              </Form.Control>
+            </Form.Field>
+            <Form.Field className="FormField" name="email">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "baseline",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Form.Label className="FormLabel">Email</Form.Label>
+                <Form.Message className="FormMessage" match="valueMissing">
+                  Por favor ingrese su email
+                </Form.Message>
+                <Form.Message className="FormMessage" match="typeMismatch">
+                  Por favor ingrese un email válido
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <input className="Input" type="email" name="email" value={formData.email} onChange={handleChange} required />
+              </Form.Control>
+            </Form.Field>
+            <Form.Field className="FormField" name="mobile_phone">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "baseline",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Form.Label className="FormLabel">Teléfono Móvil</Form.Label>
+                <Form.Message className="FormMessage" match="valueMissing">
+                  Por favor ingrese su teléfono móvil
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <input className="Input" type="text" name="mobile_phone" value={formData.mobile_phone} onChange={handleChange} required />
+              </Form.Control>
+            </Form.Field>
+
+          </div>
+          <div style={{ flex: 1, paddingRight: "10px" }}>
+            <Form.Field className="FormField" name="interestProgram">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "baseline",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Form.Label className="FormLabel">Programa de Interés</Form.Label>
+                <select name="interestProgram" value={formData.interestProgram} onChange={handleChange} required>
+                  {DummyPrograms.map(program => (
+                    <option key={program._id} value={program._id}>
+                      {program.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </Form.Field>
+            <Form.Field className="FormField" name="status">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "baseline",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Form.Label className="FormLabel">Estado</Form.Label>
+                <select name="status" value={formData.status} onChange={handleChange}>
+                  <option value="active">Activo</option>
+                  <option value="inactive">Inactivo</option>
+                </select>
+              </div>
+            </Form.Field>
+            <Form.Field className="FormField" name="trackings">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "baseline",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Form.Label className="FormLabel">Trackings</Form.Label>
+              </div>
+              {trackings.map((tracking, index) => (
+                <div key={index}>
+                  <Form.Control asChild>
+                    <input
+                      className="Input"
+                      type="text"
+                      name="tracking"
+                      value={tracking.tracking}
+                      onChange={(e) => handleChange(e, index)}
+                      placeholder="Tracking"
+                      required
+                    />
+                  </Form.Control>
+                  <Form.Control asChild>
+                    <input
+                      className="Input"
+                      type="text"
+                      name="description"
+                      value={tracking.description}
+                      onChange={(e) => handleChange(e, index)}
+                      placeholder="Description"
+                      required
+                    />
+                  </Form.Control>
+                  <Form.Control asChild>
+                    <input
+                      className="Input"
+                      type="text"
+                      name="name"
+                      value={tracking.name}
+                      onChange={(e) => handleChange(e, index)}
+                      placeholder="Name"
+                      required
+                    />
+                  </Form.Control>
+                </div>
+              ))}
+            </Form.Field>
+          </div>
         </div>
-        <div>
-          <label>Nombre:</label>
-          <input type="text" name="first_name" value={formData.first_name} onChange={handleChange} required />
-        </div>
-        <div>
-          <label>Apellido:</label>
-          <input type="text" name="last_name" value={formData.last_name} onChange={handleChange} required />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-        </div>
-        <div>
-          <label>Teléfono Móvil:</label>
-          <input type="text" name="mobile_phone" value={formData.mobile_phone} onChange={handleChange} required />
-        </div>
-        <div>
-          <label>Programa de Interés:</label>
-          <select name="interestProgram" value={formData.interestProgram} onChange={handleChange} required>
-            {DummyPrograms.map(program => (
-              <option key={program._id} value={program._id}>
-                {program.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Estado:</label>
-          <select name="status" value={formData.status} onChange={handleChange}>
-            <option value="active">Activo</option>
-            <option value="inactive">Inactivo</option>
-          </select>
-        </div>
-        <div>
-          <label>Trackings:</label>
-          {trackings.map((tracking, index) => (
-            <div key={index}>
-              <input
-                type="text"
-                name="tracking"
-                value={tracking.tracking}
-                onChange={(e) => handleChange(e, index)}
-                placeholder="Tracking"
-                required
-              />
-              <input
-                type="text"
-                name="description"
-                value={tracking.description}
-                onChange={(e) => handleChange(e, index)}
-                placeholder="Description"
-                required
-              />
-              <input
-                type="text"
-                name="name"
-                value={tracking.name}
-                onChange={(e) => handleChange(e, index)}
-                placeholder="Name"
-                required
-              />
-              <input
-                type="checkbox"
-                name="new"
-                checked={tracking.new}
-                onChange={(e) => handleChange(e, index)}
-              />
-            </div>
-          ))}
-        </div>
-        <button type="submit">Guardar Lead</button>
-      </form>
+        <Form.Submit asChild>
+          <button className="Button" style={{ marginTop: 10 }}>
+            Guardar Lead
+          </button>
+        </Form.Submit>
+      </Form.Root>
       <h2 className="flex text-2xl font-title text-purple-800">Lista de Leads</h2>
-      <table>
+      <table style={{ borderCollapse: "collapse", width: "100%" }}>
         <thead>
           <tr>
-            <th>Nombre Completo</th>
-            <th>Email</th>
-            <th>Teléfono Móvil</th>
-            <th>Programa de Interés</th>
-            <th>Estado</th>
+            <th style={{ border: "1px solid black", padding: "8px" }}>Nombre Completo</th>
+            <th style={{ border: "1px solid black", padding: "8px" }}>Email</th>
+            <th style={{ border: "1px solid black", padding: "8px" }}>Teléfono Móvil</th>
+            <th style={{ border: "1px solid black", padding: "8px" }}>Programa de Interés</th>
+            <th style={{ border: "1px solid black", padding: "8px" }}>Estado</th>
           </tr>
         </thead>
         <tbody>
           {leads.map((lead) => (
             <tr key={lead._id}>
-              <td>{lead.full_name}</td>
-              <td>{lead.email}</td>
-              <td>{lead.mobile_phone}</td>
-              <td>{DummyPrograms.find(program => program._id === lead.interestProgram)?.name || "N/A"}</td>
-              <td>{lead.status}</td>
+              <td style={{ border: "1px solid black", padding: "8px" }}>{lead.full_name}</td>
+              <td style={{ border: "1px solid black", padding: "8px" }}>{lead.email}</td>
+              <td style={{ border: "1px solid black", padding: "8px" }}>{lead.mobile_phone}</td>
+              <td style={{ border: "1px solid black", padding: "8px" }}>{DummyPrograms.find(program => program._id === lead.interestProgram)?.name || "N/A"}</td>
+              <td style={{ border: "1px solid black", padding: "8px" }}>{lead.status}</td>
             </tr>
           ))}
         </tbody>
